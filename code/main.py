eb5463631a1756261983cc0d76d1bd65248d47a7
Matías Miranda <matias.miranda.ar@gmail.com>
2025-06-18 01:34:25 -0300

    Fix Cloud Function

--- code/main.py
+++ code/main.py
@@ -1,31 +1,36 @@
-#PARA USAR EN LA CLOUD FUNCTION
+# PARA USAR EN LA CLOUD FUNCTION
+# En requirements.txt
+# functions-framework==3.*
+# google-cloud-storage
+# google-cloud-bigquery
+
+import functions_framework
 from google.cloud import bigquery
-from google.cloud import storage
 
-project_name = 'fantina-v1'
-bigquery_dataset = 'Test'
+project_name = "poc-bq-vertex-450119"
+bigquery_dataset = "test_ucema"
 
-def mover_dataset_x(event, context):
-    file = event
-    file_name = file['name']
-    table_name = file_name.split('_')[0]
-    print("Se detecto que se subio el archivo: " + file_name)
-    source_uri = "gs://" + file['bucket'] + "/" + file_name
+
+# CloudEvent trigger (para funciones 2nd gen con Eventarc + Cloud Storage)
+@functions_framework.cloud_event
+def mover_dataset_x(event):
+    data = event.data
+    file_name = data["name"]
+    bucket_name = data["bucket"]
+
+    print(f"Se detectó que se subió el archivo: {file_name}")
+    source_uri = f"gs://{bucket_name}/{file_name}"
+    table_name = file_name.split("_")[0]
+    table_id = f"{project_name}.{bigquery_dataset}.{table_name}"
 
     client = bigquery.Client(project=project_name)
-    table_id = project_name + "." + bigquery_dataset + "." + table_name
-
     job_config = bigquery.LoadJobConfig(
         autodetect=True,
         skip_leading_rows=1,
         source_format=bigquery.SourceFormat.CSV,
     )
 
-    load_job = client.load_table_from_uri(
-        source_uri, table_id, job_config=job_config
-    )  # Make an API request.
+    load_job = client.load_table_from_uri(source_uri, table_id, job_config=job_config)
+    load_job.result()  # Espera a que termine
 
-    load_job.result()  # Waits for the job to complete.
-
-    #destination_table = client.get_table(table_id)  # Make an API request.
-    print("Se cargo la tabla: " + table_name + " en BigQuery")
+    print(f"✅ Se cargó la tabla '{table_name}' en BigQuery desde {source_uri}")
